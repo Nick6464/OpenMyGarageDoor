26bf8a92adacf202c079aee3d3e6c41d4070c7ef
ducky-V <izaakkarasawa@gmail.com>
2022-03-09 20:25:10 +1300

    Theme.tsx: added theme
    Added the basic properties for a dark theme.

--- frontend/src/components/Theme.tsx
+++ frontend/src/components/Theme.tsx
@@ -1,2 +1,18 @@
-import React from "react";
+import { createTheme } from "@mui/material/styles";
 
+const darkTheme = createTheme ({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#5893df',
+    },
+    secondary: {
+      main: '#2ec5d3',
+    },
+    background: {
+      default: '#192231',
+    },
+  },
+});
+
+export default darkTheme;
